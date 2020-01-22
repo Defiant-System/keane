d3f0ec72ad6a94b7b753a2c93d8509487e505e07
Hakan Bilgin <hbi@longscript.com>
2020-01-22 14:07:14 +0100

    added zooming functionality to see translucency

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,36 @@ const photoshop = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+		this.canvas = window.find(".canvas");
+		this.thumbRect = window.find(".thumb-wrapper .view-rect");
 		this.zoomSlider = window.find(".zoom-slider input");
 
 		// bind event handlers
 		this.zoomSlider.on("input", this.dispatch);
 	},
 	dispatch(event) {
+		let self = photoshop,
+			percent;
+
 		switch (event.type) {
 			case "input":
-				console.log(this.value);
+				percent = this.value / 100;
+
+				self.canvas.css({
+					width: (600 * percent) +"px",
+					height: (366 * percent) +"px",
+				});
+
+				let width = Math.min((190 * 1.25) / percent, 190);
+				let height = Math.min((120 * 1.25) / percent, 120);
+				let top = (120 - height) / 2;
+				let left = (190 - width) / 2;
+				self.thumbRect.css({
+					top: top +"px",
+					left: left +"px",
+					width: width +"px",
+					height: height +"px",
+				});
 				break;
 			case "window.open":
 				break;
