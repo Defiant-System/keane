122544038fefe53c8ff4fcf022fb1ffb25a68812
Hakan Bilgin <hbi@longscript.com>
2020-01-22 12:47:12 +0100

    switching to headless UI

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,16 @@ const photoshop = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+		this.zoomSlider = window.find(".zoom-slider input");
+
+		// bind event handlers
+		this.zoomSlider.on("input", this.dispatch);
 	},
 	dispatch(event) {
 		switch (event.type) {
+			case "input":
+				console.log(this.value);
+				break;
 			case "window.open":
 				break;
 		}
