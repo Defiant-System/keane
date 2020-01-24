2b94fb8983fbcbb7c403b5d07946ee7e2345d6ff
Hakan Bilgin <hbi@longscript.com>
2020-01-24 14:08:59 +0100

    color box

--- src/js/main.js
+++ src/js/main.js
@@ -4,17 +4,19 @@ const photoshop = {
 		// fast references
 		this.content = window.find("content");
 		this.canvas = window.find(".canvas");
-		this.thumbRect = window.find(".thumb-wrapper .view-rect");
+		this.boxNavigator = window.find(".navigator-wrapper");
 		this.zoomSlider = window.find(".zoom-slider input");
-		this.zoomValue = window.find(".box-foot .value");
+
+		// auto store 'box-characters.htm'
+		window.store("box-navigator.htm", ".navigator-wrapper");
+		window.store("box-characters.htm", ".character-wrapper");
 
 		// bind event handlers
 		this.zoomSlider.on("input", this.dispatch);
 
-		// auto store 'box-characters.htm'
-		window.store("box-characters.htm", ".character-wrapper");
-
-		this.zoomSlider.val(235).trigger("input");
+		// temp
+		//this.zoomSlider.val(235).trigger("input");
+		window.find('[data-content="box-colors.htm"]').trigger("click");
 	},
 	dispatch(event) {
 		let self = photoshop,
@@ -25,7 +27,7 @@ const photoshop = {
 			case "input":
 				let percent = this.value / 100;
 
-				self.zoomValue.html(this.value + "%");
+				window.find(".box-foot .value").html(this.value + "%");
 
 				self.canvas.css({
 					width: (600 * percent) +"px",
@@ -34,10 +36,10 @@ const photoshop = {
 
 				let width = Math.min((190 * 1.25) / percent, 190),
 					height = Math.min((120 * 1.25) / percent, 120),
-					top = ((120 - height) / 2) - 1,
-					left = ((190 - width) / 2) - 1;
+					top = ((120 - height) / 2),
+					left = ((190 - width) / 2);
 
-				self.thumbRect.css({
+				self.boxNavigator.find(".view-rect").css({
 					top: top +"px",
 					left: left +"px",
 					width: width +"px",
@@ -46,7 +48,7 @@ const photoshop = {
 				break;
 			case "change-bg":
 				self.canvas.css({"background-image": `url('/cdn/img/bg/${event.arg}.jpg')`});
-				self.thumbRect.parent().css({"background-image": `url('/cdn/img/bg/${event.arg}.jpg')`});
+				self.boxNavigator.css({"background-image": `url('/cdn/img/bg/${event.arg}.jpg')`});
 				break;
 			case "box-head-tab":
 				el = $(event.target);
