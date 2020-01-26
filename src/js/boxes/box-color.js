ba98bb9ad71f474af1878d80998989d3bd4e1c3a
Hakan Bilgin <hbi99@hotmail.com>
2020-01-26 18:26:00 +0100

    color wheel events

--- src/js/boxes/box-color.js
+++ src/js/boxes/box-color.js
@@ -8,21 +8,68 @@
 			this.wrapper = el.find(".wheel-object");
 			this.wheel = el.find(".color-wheel");
 			this.triangle = el.find(".color-triangle");
+			this.dot = el.find(".color-dot");
 
 			// bind event handlers
-			this.wheel.on("mousedown", this.dispatch)
+			this.wheel.on("mousedown", this.wheelEvents)
+			this.triangle.on("mousedown", this.triangleEvents)
 		} else {
-
 			// unbind event handlers
-			this.wheel.off("mousedown", this.dispatch)
+			this.wheel.off("mousedown", this.wheelEvents)
+			this.triangle.off("mousedown", this.triangleEvents)
 		}
 	},
-	dispatch(event) {
+	triangleEvents(event) {
 		let root = photoshop,
 			self = root.box.color,
-			dx,
-			dy,
+			down = self.triangleDown,
+			top,
+			left;
+
+		switch (event.type) {
+			case "mousedown":
+				// stop event from bubbling up
+				event.stopPropagation();
+
+				self.triangleDown = {
+					offsetY: self.wrapper[0].offsetTop + 20,
+					offsetX: self.wrapper[0].offsetLeft + 22,
+					clickY: event.clientY,
+					clickX: event.clientX,
+				};
+
+				self.dot.css({
+					top: (event.offsetY + 20) +"px",
+					left: (event.offsetX + 22) +"px",
+				});
+
+				// bind event handlers
+				self.triangle.on("mousemove mouseup mouseout", self.triangleEvents)
+				break;
+			case "mousemove":
+				top = event.clientY - down.clickY + event.offsetY;
+				left = event.clientX - down.clickX + event.offsetX;
+
+				self.dot.css({
+					top: top +"px",
+					left: left +"px",
+				});
+				break;
+			case "mouseout":
+			case "mouseup":
+				// unbind event handlers
+				self.triangle.off("mousemove mouseup mouseout", self.triangleEvents)
+				break;
+		}
+	},
+	wheelEvents(event) {
+		let root = photoshop,
+			self = root.box.color,
+			dx, dy,
+			style,
+			speed,
 			angle,
+			diff,
 			hue;
 		switch (event.type) {
 			case "mousedown":
@@ -30,32 +77,41 @@
 				dx = event.offsetX - 128;
 				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
 				hue = (360 - angle) % 360;
+				// calculate angle diff
+				diff = ((this.cAngle || 0) - angle + 180) % 360 - 180;
+				diff = diff < -180 ? diff - 180 : diff;
+
+				speed = Math.abs(diff) * 2.5;
+				angle = (this.cAngle || 0) - diff;
+				style = `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg); --speed: ${speed}ms;`;
+				this.cAngle = angle;
 
 				self.wrapper
 					.cssSequence("fade", "transitionend", i => self.wrapper.removeClass("fade"))
-					.attr({
-						style: `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg);`
-					});
+					.attr({ style });
 
-				self.down = true;
-				self.wheel.on("mousemove mouseup", self.dispatch)
+				// bind event handlers
+				self.wheel.on("mousemove mouseup mouseout", self.wheelEvents)
+				self.wheelDown = true;
 				break;
 			case "mousemove":
-				if (!self.down) return;
+				//if (!self.wheelDown) return;
+				self.wrapper.removeClass("fade");
 
 				dy = event.offsetY - 128;
 				dx = event.offsetX - 128;
 				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
 				hue = (360 - angle) % 360;
-				//console.log(hue);
+				style =  `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg);`;
+				this.cAngle = angle;
 
-				self.wrapper.attr({
-					style: `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg);`
-				});
+				self.wrapper.attr({ style });
 				break;
+			case "mouseout":
 			case "mouseup":
-				delete self.down;
-				self.wheel.off("mousemove mouseup", self.dispatch)
+				// unbind event handlers
+				self.wheel.off("mousemove mouseup mouseout", self.wheelEvents)
+				//delete self.wheelDown;
 				break;
 		}
 	}
