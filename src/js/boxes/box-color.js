18fb5e54305771f6432e849f9bfe181774965a28
Hakan Bilgin <hbi99@hotmail.com>
2020-01-26 10:25:52 +0100

    wheel rotation

--- src/js/boxes/box-color.js
+++ src/js/boxes/box-color.js
@@ -10,19 +10,40 @@
 			this.triangle = el.find(".color-triangle");
 
 			// bind event handlers
-			this.triangle.on("mousedown", this.dispatch)
+			this.wheel.on("mousedown", this.dispatch)
 		} else {
 
 			// unbind event handlers
-			this.triangle.off("mousedown", this.dispatch)
+			this.wheel.off("mousedown", this.dispatch)
 		}
 	},
 	dispatch(event) {
 		let root = photoshop,
-			self = root.box.color;
+			self = root.box.color,
+			dx,
+			dy,
+			angle,
+			hue;
 		switch (event.type) {
 			case "mousedown":
-				console.log(event);
+				self.down = true;
+				self.wheel.on("mousemove mouseup", self.dispatch)
+				break;
+			case "mousemove":
+				if (!self.down) return;
+
+				dy = event.offsetY - 128;
+				dx = event.offsetX - 128;
+				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
+				hue = (-angle - 39);
+
+				self.wrapper.attr({
+					style: `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg);`
+				});
+				break;
+			case "mouseup":
+				delete self.down;
+				self.wheel.off("mousemove mouseup", self.dispatch)
 				break;
 		}
 	}
