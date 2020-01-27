a7996e0d527a4e9b3479aab27e310779db0135e8
Hakan Bilgin <hbi@longscript.com>
2020-01-27 13:05:01 +0100

    color box ui + ux

--- src/js/boxes/box-color.js
+++ src/js/boxes/box-color.js
@@ -7,22 +7,22 @@
 			// fast references
 			this.wheelObject = el.find(".wheel-object");
 			this.wheel = el.find(".color-wheel");
-			this.triangle = el.find(".color-triangle");
+			this.shape = el.find(".color-shape");
 			this.dot = el.find(".color-dot");
 
 			// bind event handlers
 			this.wheel.on("mousedown", this.wheelEvents)
-			this.triangle.on("mousedown", this.triangleEvents)
+			//this.shape.on("mousedown", this.shapeEvents)
 		} else {
 			// unbind event handlers
 			this.wheel.off("mousedown", this.wheelEvents)
-			this.triangle.off("mousedown", this.triangleEvents)
+			//this.shape.off("mousedown", this.shapeEvents)
 		}
 	},
-	triangleEvents(event) {
+	shapeEvents(event) {
 		let root = photoshop,
 			self = root.box.color,
-			down = self.triangleDown,
+			down = self.shapeDown,
 			shape,
 			point;
 
@@ -35,7 +35,7 @@
 				//console.log( event.offsetX, event.offsetY );
 				shape = new Polygon([[38,7], [185,90], [38,171]]);
 
-				self.triangleDown = {
+				self.shapeDown = {
 					shape,
 					offsetY: event.offsetY,
 					offsetX: event.offsetX,
@@ -43,10 +43,10 @@
 					clickX: event.clientX,
 				};
 
-				//console.log(self.triangleDown.shape.getCentroid());
+				//console.log(self.shapeDown.shape.getCentroid());
 
 				//fake trigger event
-				self.triangleEvents({
+				self.shapeEvents({
 					type: "mousemove",
 					clientY: event.clientY,
 					clientX: event.clientX,
@@ -54,7 +54,7 @@
 
 				// bind event handlers
 				self.wheelObject
-					.on("mousemove mouseup mouseout", self.triangleEvents);
+					.on("mousemove mouseup mouseout", self.shapeEvents);
 				break;
 			case "mousemove":
 				point = down.shape.constrain([
@@ -72,7 +72,7 @@
 			case "mouseup":
 				// unbind event handlers
 				self.wheelObject
-					.off("mousemove mouseup mouseout", self.triangleEvents);
+					.off("mousemove mouseup mouseout", self.shapeEvents);
 				break;
 		}
 	},
@@ -90,16 +90,19 @@
 				// prevent default behaviour
 				event.preventDefault();
 
-				dy = event.offsetY - 128;
-				dx = event.offsetX - 128;
+				dy = event.offsetY - 83;
+				dx = event.offsetX - 83;
 				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
 				hue = (360 - angle) % 360;
-				// calculate angle diff
-				diff = ((this.cAngle || 0) - angle + 180) % 360 - 180;
-				diff = diff < -180 ? diff - 360 : diff;
 
-				speed = Math.abs(diff) * 2.5;
-				angle = (this.cAngle || 0) - diff;
+				// return console.log(hue);
+
+				// // calculate angle diff
+				// diff = ((this.cAngle || 0) - angle + 180) % 360 - 180;
+				// diff = diff < -180 ? diff - 180 : diff;
+
+				// speed = Math.abs(diff) * 2.5;
+				// angle = (this.cAngle || 0) - diff;
 				style = `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg); --speed: ${speed}ms;`;
 				this.cAngle = angle;
 
@@ -107,28 +110,11 @@
 					.cssSequence("fade", "transitionend", i => self.wheelObject.removeClass("fade"))
 					.attr({ style });
 
-				// bind event handlers
-				self.wheel.on("mousemove mouseup mouseout", self.wheelEvents);
-				self.wheelDown = true;
 				break;
 			case "mousemove":
-				//if (!self.wheelDown) return;
-				self.wheelObject.removeClass("fade");
-
-				dy = event.offsetY - 128;
-				dx = event.offsetX - 128;
-				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
-				hue = (360 - angle) % 360;
-				style =  `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg);`;
-				this.cAngle = angle;
-
-				self.wheelObject.attr({ style });
 				break;
 			case "mouseout":
 			case "mouseup":
-				// unbind event handlers
-				self.wheel.off("mousemove mouseup mouseout", self.wheelEvents);
-				//delete self.wheelDown;
 				break;
 		}
 	}
