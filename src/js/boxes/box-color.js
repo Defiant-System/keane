25d233a0b6e6c849baa97fe19463b8568c1c88a1
Hakan Bilgin <hbi@longscript.com>
2020-01-29 15:41:25 +0100

    resturcturing UI

--- src/js/boxes/box-color.js
+++ src/js/boxes/box-color.js
@@ -14,6 +14,13 @@
 			// bind event handlers
 			this.wheel.on("mousedown", this.wheelEvents)
 			//this.shape.on("mousedown", this.shapeEvents)
+
+		//	setTimeout(() => this.dispatch({
+		//			type: "set-color-hsl",
+		//		//	arg: "hsl(0, 100%, 100%)"
+		//		//	arg: "hsl(270, 90%, 31%)"
+		//			arg: "hsl(91, 100%, 100%)"
+		//		}), 1000);
 		} else {
 			// unbind event handlers
 			this.wheel.off("mousedown", this.wheelEvents)
@@ -22,9 +29,35 @@
 	},
 	dispatch(event) {
 		let root = photoshop,
-			self = root.box.color;
+			self = root.box.color,
+			rx,
+			values;
 
 		switch (event.type) {
+			case "set-color-hsl":
+				rx = /hsl\((\d+),\s*([\d.]+)%,\s*([\d.]+)%\)/g
+				values = rx.exec(event.arg);
+				if (!values) return;
+
+				let hue = values[1],
+					saturation = values[2],
+					brightness = values[3],
+					angle = hue,
+					speed = 250;
+
+				self.el.find("input[name='color-hsl-hue']").val(hue +"°");
+				self.el.find("input[name='color-hsl-saturation']").val(saturation +"%");
+				self.el.find("input[name='color-hsl-lightness']").val(brightness +"%");
+
+
+				style = `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg); --speed: ${speed}ms;`;
+
+				// shape rotation
+				self.wheelObject
+					.cssSequence("easeTo", "transitionend", i => self.wheelObject.removeClass("easeTo"))
+					.attr({ style });
+
+				break;
 			case "show-color-values":
 				event.el.parent().find(".active").removeClass("active");
 				event.el.addClass("active");
@@ -40,6 +73,45 @@
 				self.el.find(".color-wheel .color-shape")
 					.removeClass("triangle rectangle")
 					.addClass(event.arg);
+				break;
+		}
+	},
+	wheelEvents(event) {
+		let root = photoshop,
+			self = root.box.color,
+			diff = 0,
+			dx, dy,
+			angle,
+			style,
+			speed,
+			hue;
+		switch (event.type) {
+			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+
+				dy = event.offsetY - 83;
+				dx = event.offsetX - 83;
+				
+				if (Math.sqrt(dy * dy + dx * dx) <= 66) return; // event passed to shapeEvents
+
+				angle = Math.round(Math.atan2(dy, dx) * 180 / Math.PI);
+				hue = angle < 0 ? 360 + angle : angle;
+
+				speed = 200; //Math.max(Math.abs(diff) * 2.5, 200);
+				style = `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg); --speed: ${speed}ms;`;
+
+				// shape rotation
+				self.wheelObject
+					.cssSequence("easeTo", "transitionend", i => self.wheelObject.removeClass("easeTo"))
+					.attr({ style });
+
+				self.el.find("input[name='color-hsl-hue']").val(hue +"°");
+				break;
+			case "mousemove":
+				break;
+			case "mouseout":
+			case "mouseup":
 				break;
 		}
 	},
@@ -97,48 +169,6 @@
 				// unbind event handlers
 				self.wheelObject
 					.off("mousemove mouseup mouseout", self.shapeEvents);
-				break;
-		}
-	},
-	wheelEvents(event) {
-		let root = photoshop,
-			self = root.box.color,
-			dx, dy,
-			style,
-			speed,
-			angle,
-			diff,
-			hue;
-		switch (event.type) {
-			case "mousedown":
-				// prevent default behaviour
-				event.preventDefault();
-
-				dy = event.offsetY - 83;
-				dx = event.offsetX - 83;
-				angle = parseInt(Math.atan2(dy, dx) * (180 / Math.PI), 10);
-				hue = (360 - angle) % 360;
-
-				// return console.log(hue);
-
-				// // calculate angle diff
-				// diff = ((this.cAngle || 0) - angle + 180) % 360 - 180;
-				// diff = diff < -180 ? diff - 180 : diff;
-
-				// speed = Math.abs(diff) * 2.5;
-				// angle = (this.cAngle || 0) - diff;
-				style = `--color: hsl(${hue}, 100%, 50%); --rotation: rotate(${angle}deg); --speed: ${speed}ms;`;
-				this.cAngle = angle;
-
-				self.wheelObject
-					.cssSequence("fade", "transitionend", i => self.wheelObject.removeClass("fade"))
-					.attr({ style });
-
-				break;
-			case "mousemove":
-				break;
-			case "mouseout":
-			case "mouseup":
 				break;
 		}
 	}
