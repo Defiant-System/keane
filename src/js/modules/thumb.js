00dbf7e059a0869d3e48af0a35f5e996497c0baa
Hakan Bilgin <hbi99@hotmail.com>
2020-06-29 13:37:40 +0200

    thumbnails for channels box

--- src/js/modules/thumb.js
+++ src/js/modules/thumb.js
@@ -8,17 +8,42 @@ const Thumb = {
 			width = Cvs.offsetWidth,
 			height = Cvs.offsetHeight,
 			ratio = _canvas.oW / _canvas.oH,
-			l, t, w, h;
+			l, t, w, h, c;
 
 		options.el.prop({ width, height });
 
 		w = width;
 		h = _round(width / ratio);
+		if (h > height - 2) {
+			// portrait
+			h = height - 2;
+			w = _round(width * ratio);
+		}
 		l = _round((width - w) / 2);
 		t = _round((height - h) / 2);
 
+		// original canvas image
 		Ctx.drawImage(_canvas.osCvs[0], l, t, w, h);
 
+		c = ["red", "blue", "green"].indexOf(options.channel);
+		if (c > -1) {
+			let img = Ctx.getImageData(l, t, w, h),
+				data = img.data;
+
+			for (let y=0; y<h; y++) {
+				for (let x=0; x<w; x++) {
+					let o = ((y * w) + x) * 4;
+					data[o + 0] = data[o + c]; // * 0.2126;
+					data[o + 1] = data[o + c]; // * 0.7152;
+					data[o + 2] = data[o + c]; // * 0.0722;
+					data[o + 3] = data[o + 3];
+				}
+			}
+
+			Ctx.putImageData(img, l, t);
+		}
+
+		// draw borders
 		Ctx.strokeStyle = "#000";
 		Ctx.lineWidth = .5;
 
