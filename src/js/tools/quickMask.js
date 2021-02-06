ecb28491e8f9c066eb90d0e0de514d84b677de28
Hakan Bilgin <hbi99@hotmail.com>
2021-02-07 00:07:54 +0100

    quickMask toggle menu

--- src/js/tools/quickMask.js
+++ src/js/tools/quickMask.js
@@ -3,7 +3,7 @@
 
 {
 	init() {
-		
+		this.enabled = false;
 	},
 	dispatch(event) {
 		let APP = keane,
@@ -12,6 +12,17 @@
 		switch (event.type) {
 			// custom events
 			case "enable":
+				Self.enabled = !Self.enabled;
+				// toggle menu item
+				if (Self.enabled) {
+					window.bluePrint.selectSingleNode(`//Menu[@click="tool:quickMask:enable"]`).setAttribute("type", "hidden");
+					window.bluePrint.selectSingleNode(`//Menu[@click="tool:quickMask:disable"]`).removeAttribute("type");
+				} else {
+					window.bluePrint.selectSingleNode(`//Menu[@click="tool:quickMask:enable"]`).removeAttribute("type");
+					window.bluePrint.selectSingleNode(`//Menu[@click="tool:quickMask:disable"]`).setAttribute("type", "hidden");
+					return Self.dispatch({ type: "disable" });
+				}
+
 				console.log(event);
 				break;
 			case "disable":
