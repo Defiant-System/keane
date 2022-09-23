02a3a4e60e1a370b807706dc93a200c1019ca879
Hakan Bilgin <hbi99@hotmail.com>
2022-09-23 22:24:14 +0200

    Update circles.js

--- src/js/tools/feather/circles.js
+++ src/js/tools/feather/circles.js
@@ -23,7 +23,24 @@ class Circles {
 	}
 
 	stroke(mX, mY) {
+		let dx = mX - this.mX;
+		let dy = mY - this.mY;
+		let d = Math.sqrt(dx * dx + dy * dy) * 2;
 		
+		let cx = Math.floor(mX / 100) * 100 + 50;
+		let cy = Math.floor(mY / 100) * 100 + 50;
+		
+		let steps = Math.floor( Math.random() * 10 );
+		let step_delta = d / steps;
+
+		for (let i = 0; i < steps; i++) {
+			this.ctx.beginPath();
+			this.ctx.arc( cx, cy, (steps - i) * step_delta, 0, Math.PI*2, true);
+			this.ctx.stroke();
+		}
+
+		this.mX = mX;
+		this.mY = mY;
 	}
 
 	strokeEnd() {
