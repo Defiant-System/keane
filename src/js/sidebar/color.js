01c43502f9722758a08d5b4cc722ce542c6a3319
Hakan Bilgin <hbi99@hotmail.com>
2022-09-26 08:59:11 +0200

    sidebar color box

--- src/js/sidebar/color.js
+++ src/js/sidebar/color.js
@@ -3,7 +3,14 @@
 
 {
 	init() {
+		let root = window.find(`.box-body > div[data-box="color"]`);
+		this.els = {
+			root,
+			colorValues: root.find(".color-values"),
+		};
 
+		// subscribe to events
+		karaqu.on("set-fg-color", this.dispatch);
 	},
 	dispatch(event) {
 		let APP = keane,
@@ -11,9 +18,19 @@
 			el;
 		// console.log(event);
 		switch (event.type) {
+			// subscribed events
+			case "set-fg-color":
+				console.log( event.detail.hex );
+				break;
+
 			// custom events
 			case "show-color-values":
-				console.log(event);
+				event.el.parent().find(".active").removeClass("active");
+				event.el.addClass("active");
+				
+				Self.els.colorValues
+					.removeClass("show-rgb show-hsl")
+					.addClass(`show-${event.arg}`);
 				break;
 			case "color-wheel-shape":
 				// TODO (!?)
