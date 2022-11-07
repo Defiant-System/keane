2bfa18e81ba3e456a5d04e70dc512149e7844d74
Hakan Bilgin <hbi99@hotmail.com>
2022-11-07 12:10:06 +0100

    UI/UX svg rect radius change

--- src/js/tools/shape.js
+++ src/js/tools/shape.js
@@ -7,13 +7,13 @@
 		this.handleBox = keane.els.handleBox;
 
 		this.boxType = {
-			circle: "rectangle",
-			ellipse: "rectangle",
+			circle: "box",
+			ellipse: "box",
 			rect: "rectangle",
-			polygon: "rectangle",
-			polyline: "rectangle",
-			path: "rectangle",
-			image: "rectangle",
+			polygon: "box",
+			polyline: "line",
+			path: "box",
+			image: "box",
 			line: "line",
 			bezier: "bezier",
 		};
@@ -46,14 +46,6 @@
 				break;
 		}
 	},
-	isLine(shape) {
-		let lineItem = shape.find(this.shapeTypes.join(",")),
-			name = lineItem.prop("nodeName"),
-			d = (name === "path") ? lineItem.attr("d").split(" ") : false,
-			type = name === "line" ? "line" : "shape";
-		if (d && d.length === 4) type = "line";
-		return type === "line";
-	},
 	doMove(event) {
 		let APP = keane,
 			Self = APP.tools.shape,
@@ -63,14 +55,20 @@
 				// prevent default behaviour
 				event.preventDefault();
 
-				let el = $(event.target);
+				let el = $(event.target),
+					names = Object.keys(Self.boxType).join(",");
+				// reset reference
+				Self.svgItem = undefined;
+				Self.shape = undefined;
 				
 				if (!el.hasClass("shape")) el = el.parents(".shape");
 				if (!el.length) return Self.handleBox.removeClass("show");
+				// reference to selected shape
+				Self.svgItem = el;
+				Self.shape = el.find(names);
 
 				// UI update handle-box
-				let names = Object.keys(Self.boxType).join(","),
-					child = el.find(names).get(0),
+				let child = el.find(names).get(0),
 					name = child.prop("nodeName");
 				if (name === "path" && child.attr("d").split(" ").length === 4) {
 					name = "bezier";
@@ -132,14 +130,134 @@
 				break;
 		}
 	},
+	doRadius(event) {
+		let APP = keane,
+			Self = APP.tools.shape,
+			Drag = Self.drag;
+		switch (event.type) {
+			case "mousedown":
+				let File = Projector.file,
+					el = $(event.target),
+					pEl = el.parent(),
+					type = el.prop("className").split(" ")[2],
+					shape = Self.shape,
+					[t, l, vW, vH] = Self.svgItem.attr("viewBox").split(" "),
+					offset = {
+						l: +el.prop("offsetLeft"),
+						t: +el.prop("offsetTop"),
+						x: parseInt(shape.css("x"), 10),
+						w: parseInt(shape.css("width"), 10),
+						h: parseInt(shape.css("height"), 10),
+					},
+					ratio = offset.w / offset.h,
+					origo = {
+						x: vW >> 1,
+						y: vH >> 1,
+						r: (Math.min(offset.w, offset.h) >> 1),
+					};
+				// calculate origo for handles
+				if (ratio != 1) {
+					switch (type) {
+						case "ne":
+							origo.x = ratio > 1 ? vH >> 1 : origo.x;
+							origo.y = ratio < 1 ? vW >> 1 : origo.y;
+							break;
+						case "nw":
+							origo.x = ratio > 1 ? vW - (vH >> 1) : origo.x;
+							origo.y = ratio < 1 ? vW >> 1 : origo.y;
+							break;
+						case "sw":
+							origo.x = ratio > 1 ? vW - (vH >> 1) : origo.x;
+							origo.y = ratio < 1 ? vH - (vW >> 1) : origo.y;
+							break;
+						case "se":
+							origo.x = ratio > 1 ? vH >> 1 : origo.x;
+							origo.y = ratio < 1 ? vH - (vW >> 1) : origo.y;
+							break;
+					}
+				}
+				// create drag object
+				Self.drag = {
+					el,
+					pEl,
+					shape,
+					type,
+					origo,
+					offset,
+					click: {
+						x: event.clientX - offset.l,
+						y: event.clientY - offset.t,
+					},
+					getRadius(x, y) {
+						let min = Math.min,
+							o = this.origo,
+							v;
+						switch (this.type) {
+							case "ne": v = min(o.y-y, o.x-x, o.r); break;
+							case "nw": v = min(o.y-y, x-o.x, o.r); break;
+							case "sw": v = min(y-o.y, x-o.x, o.r); break;
+							case "se": v = min(y-o.y, o.x-x, o.r); break;
+						}
+						return min(Math.max(o.r-v, 0), o.r);
+					},
+				};
+
+				// hide from layer & show SVG version
+				Self.svgItem.addClass("transforming");
+				// re-render layer
+				File.activeLayer.renderShapes({ noEmit: true });
+
+				// cover layout
+				APP.els.content.addClass("cover no-cursor");
+				// bind event
+				UI.doc.on("mousemove mouseup", Self.doRadius);
+				break;
+			case "mousemove":
+				let x = event.clientX - Drag.click.x,
+					y = event.clientY - Drag.click.y,
+					rx = Drag.getRadius(x, y);
+				Drag.pEl.css({ "--rc": (rx-4) +"px" });
+				Drag.shape.attr({ rx: rx });
+				break;
+			case "mouseup":
+				// re-render layer
+				Projector.file.activeLayer.renderShapes({ all: true });
+				// uncover layout
+				APP.els.content.removeClass("cover no-cursor");
+				// unbind event
+				UI.doc.off("mousemove mouseup", Self.doRadius);
+				break;
+		}
+	},
+	doRotate(event) {
+		let APP = keane,
+			Self = APP.tools.shape,
+			Drag = Self.drag;
+		switch (event.type) {
+			case "mousedown":
+				break;
+			case "mousemove":
+				break;
+			case "mouseup":
+				break;
+		}
+	},
 	doResize(event) {
 		let APP = keane,
 			Self = APP.tools.shape,
 			Drag = Self.drag;
 		switch (event.type) {
 			case "mousedown":
+				// prevent default behaviour
+				event.preventDefault();
+
+				let el = $(event.target);
+				// proxy event, if needed
+				if (el.hasClass("rc")) return Self.doRadius(event);
+				if (el.hasClass("rot")) return Self.doRotate(event);
+
 				// prepare values
-				let el = Self.handleBox,
+				let bEl = Self.handleBox,
 					type = event.target.className.split(" ")[1],
 					min = {
 						w: 50,
@@ -150,15 +268,15 @@
 						y: event.clientY,
 					},
 					offset = {
-						x: parseInt(el.css("left"), 10),
-						y: parseInt(el.css("top"), 10),
-						w: parseInt(el.css("width"), 10),
-						h: parseInt(el.css("height"), 10),
+						x: parseInt(bEl.css("left"), 10),
+						y: parseInt(bEl.css("top"), 10),
+						w: parseInt(bEl.css("width"), 10),
+						h: parseInt(bEl.css("height"), 10),
 						// rx: +shape.attr("rx"),
 					};
 				// create drag object
 				Self.drag = {
-					el,
+					bEl,
 					type,
 					min,
 					click,
@@ -196,7 +314,7 @@
 				// apply new dimensions to element
 				if (dim.width < Drag.min.w) dim.width = Drag.min.w;
 				if (dim.height < Drag.min.h) dim.height = Drag.min.h;
-				Drag.el.css(dim);
+				Drag.bEl.css(dim);
 				break;
 			case "mouseup":
 				// uncover layout
